675f8730a869bc003c3bdf4d903cc81fa8f57ef8
David Enke <david@enke.dev>
2025-07-26 22:11:09 +0200

    chore(docs): add types for search plugin

--- globals.d.ts
+++ globals.d.ts
@@ -1,5 +1,14 @@
 declare module 'vitepress-plugin-search' {
+  import type { IndexOptions } from 'flexsearch';
   import type { Plugin } from 'vite';
 
-  export function SearchPlugin(options?: unknown): Plugin;
+  interface SearchOptions extends IndexOptions<unknown> {
+    previewLength: number;
+    buttonLabel: string;
+    placeholder: string;
+    allow: string[];
+    ignore: string[];
+  }
+
+  export function SearchPlugin(options?: Partial<SearchOptions>): Plugin;
 }
