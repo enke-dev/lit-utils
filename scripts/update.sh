71fec6a97740518c6e58e37add303ffcbfd81730
David Enke <david@enke.dev>
2026-01-11 13:10:10 +0100

    chore: update node
    and align to use `fnm` instead of `nvm`

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,12 +5,12 @@
 # brew install fnm --HEAD
 # eval "$(fnm env --use-on-cd --shell zsh)"
 
-# Update node to latest version and update .nvmrc files
+# Update node to latest version and update .node-version files
 # > this requires fnm to be installed from HEAD, as the last stable
 # > release is from 11/2024 not containing this flag yet
 echo "> Updating Node.js to current LTS version"
 fnm install --lts --use
-node -v > .nvmrc
+node -v > .node-version
 
 # Update all dependencies to latest version in all packages
 echo "> Checking for latest versions of dependencies"
