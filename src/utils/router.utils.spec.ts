7f177a0f4de46eaf13bc48826365631bf614b8ad
David Enke <david@enke.dev>
2026-03-05 10:17:54 +0100

    test(router.utils): add unit tests for goto and redirect
    Add sinon dev dependency for mocking and spying on history
    and window methods. Cover goto and redirect functions with
    tests verifying pushState/replaceState and event dispatch.

--- src/utils/router.utils.spec.ts
+++ src/utils/router.utils.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from '@open-wc/testing';
+import sinon from 'sinon';
 
-import { handleSearchParams } from './router.utils.js';
+import { goto, handleSearchParams, redirect } from './router.utils.js';
 
 describe('router.utils', () => {
   describe('handleSearchParams', () => {
@@ -47,6 +48,56 @@ describe('router.utils', () => {
       // THEN search params are merged, existing ones overridden
       expect(fromURL).to.equal('/foo.html?foo=foo&bar=123');
       expect(fromPath).to.equal('/foo.html?foo=foo&bar=123');
+    });
+  });
+
+  describe('goto', () => {
+    it('dispatches events', async () => {
+      // mock history pushState and window.dispatchEvent
+      const pushState = sinon.spy(history, 'pushState');
+      const dispatchEvent = sinon.spy(window, 'dispatchEvent');
+
+      // WHEN goto is called
+      goto('/foo');
+
+      // THEN pushState and dispatchEvent are called
+      expect(pushState.calledOnce).to.be.true;
+      expect(pushState.firstCall.firstArg).to.deep.equal({ path: '/foo' });
+      expect(dispatchEvent.calledOnceWith(sinon.match.instanceOf(CustomEvent))).to.be.true;
+      expect(dispatchEvent.firstCall.firstArg.detail).to.deep.equal({ path: '/foo' });
+
+      // restore mocks
+      pushState.restore();
+      dispatchEvent.restore();
+    });
+  });
+
+  describe('redirect', () => {
+    it('returns false', () => {
+      // WHEN redirect is called
+      const result = redirect('/foo');
+
+      // THEN false is returned
+      expect(result).to.be.false;
+    });
+
+    it('dispatches events', async () => {
+      // mock history pushState and window.dispatchEvent
+      const replaceState = sinon.spy(history, 'replaceState');
+      const dispatchEvent = sinon.spy(window, 'dispatchEvent');
+
+      // WHEN redirect is called
+      redirect('/foo');
+
+      // THEN replaceState and dispatchEvent are called
+      expect(replaceState.calledOnce).to.be.true;
+      expect(replaceState.firstCall.firstArg).to.deep.equal({ path: '/foo' });
+      expect(dispatchEvent.calledOnceWith(sinon.match.instanceOf(CustomEvent))).to.be.true;
+      expect(dispatchEvent.firstCall.firstArg.detail).to.deep.equal({ path: '/foo' });
+
+      // restore mocks
+      replaceState.restore();
+      dispatchEvent.restore();
     });
   });
 });
